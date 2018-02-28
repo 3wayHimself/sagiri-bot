7ff7694f624c418455239a6bcf51c97fc2d44f73
Kriss7475 <kriss7475@gmail.com>
2018-02-28 18:39:14 +0100

    Update to reloadgame command

--- commands/reloadgame.js
+++ commands/reloadgame.js
@@ -1,3 +1,7 @@
+const cfg = require(`../config.json`);
+
 exports.run = (m, bot, args) => {
+    if(m.author.id != cfg.oid) return;
     bot.user.setActivity(`Do ${cfg.prefix}help | ${bot.guilds.size} servers | ${bot.users.size} users`);
+    m.channel.send(`Updated.`)
 };
