220977890115e0741a52a1b447859e3ce96a34c0
Kriss7475 <kriss7475@gmail.com>
2018-03-21 21:29:11 +0100

    Fix to 8ball command. I'm blind

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,7 +1,8 @@
 const arrays = require(`../arrays.json`);
 module.exports = {
-	name: 'ping',
-    description: 'Check my latency.',
+	name: '8ball',
+    description: 'Ask a question and I will reply Yes/No/Maybe.',
+    syntax: '{question}',
     cooldown: 5,
     args: true,
 	execute(m, bot, args) {
